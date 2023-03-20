4f43e992a11c2519c2b7841d47e1b00e98532d3f
Elozor Bruce <96163908+coolcoder613eb@users.noreply.github.com>
2023-03-20 11:18:34 +1100

    start on ui

---
+++ classes.py
@@ -0,0 +1,26 @@
+class Thing:
+    def __init__(
+        self,
+        name,
+        menu=None,
+        desc=None,
+        items=None,
+        action=None,
+        places=None,
+        moveable=False,
+    ):
+        self.name = name  # for all
+        self.menu = menu  # for item
+        self.desc = desc  # for all (look)
+        self.items = items  # for all (look)
+        self.action = action  # for use
+        self.places = places  # for place (go to)
+        self.moveable = moveable  # for item (take)
+
+
+class Door:
+    def __init__(self, name, dest, desc=None, locked=False):
+        self.name = name
+        self.dest = dest
+        self.desc = desc
+        self.locked = locked
--- main.py
+++ main.py
@@ -1,42 +1,39 @@
-class Thing:
-    def __init__(
-        self,
-        name,
-        menu=None,
-        desc=None,
-        items=None,
-        action=None,
-        places=None,
-        moveable=False,
-    ):
-        self.name = name  # for all
-        self.menu = menu  # for item
-        self.desc = desc  # for all (look)
-        self.items = items  # for all (look)
-        self.action = action  # for use
-        self.places = places  # for place (go to)
-        self.moveable = moveable  # for item (take)
-
-
-class Door:
-    def __init__(self, name, dest, desc=None, locked=False):
-        self.name = name
-        self.dest = dest
-        self.desc = desc
-        self.locked = locked
+from classes import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
 
 
 def win():
     print("Win placeholder")
 
 
-doors = [Door("Cupboard door", {1: 0, 0: 1})]
+class Game(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.first = 1  # starting room
+        self.doors = [Door("Cupboard door", {1: 0, 0: 1})]
 
-places = [
-    Thing(
-        "Walk-in cupboard",
-        items=[Thing("Pile of clothes", items=[Thing("Afikoman", action=win)])],
-        places=[doors[0]],
-    ),
-    Thing("Master bedroom", places=[doors[0]]),
-]
+        self.places = [
+            Thing(
+                "Walk-in cupboard",
+                items=[Thing("Pile of clothes", items=[Thing("Afikoman", action=win)])],
+                places=[self.doors[0]],
+            ),
+            Thing("Master bedroom", places=[self.doors[0]]),
+        ]
+
+    def run(self):
+        self.name = tk.StringVar(self, self.places[self.first].name)
+        self.desc = tk.StringVar(self, self.places[self.first].desc)
+
+        self.name_label = ttk.Label(self, textvariable=self.name)
+        self.desc_label = ttk.Label(self, textvariable=self.desc)
+
+        self.name_label.grid(column=0, row=0, padx=7, pady=7)
+        self.mainloop()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
